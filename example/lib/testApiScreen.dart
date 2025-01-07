a4c854a62f746af254768bbb374ca1f2058f3125
anasOmari <anas1994omari@gmail.com>
2025-01-08 00:30:32 +0300

    upload example

---
+++ example/lib/testApiScreen.dart
@@ -0,0 +1,66 @@
+import 'package:api_request_helper/api_helper.dart';
+import 'package:flutter/material.dart';
+
+class Testapiscreen extends StatefulWidget {
+  const Testapiscreen({super.key});
+
+  @override
+  State<Testapiscreen> createState() => _TestapiscreenState();
+}
+
+class _TestapiscreenState extends State<Testapiscreen> {
+  late ApiHelper apiHelper;
+  List<dynamic> data = [];
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    apiHelper = ApiHelper(rootApi: 'https://jsonplaceholder.typicode.com');
+
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final result = await apiHelper.fetchAllData('/posts');
+      setState(() {
+        data = result;
+      });
+    } catch (e) {
+      print('Error: $e');
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test API Helper"),
+      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final item = data[index];
+                return ListTile(
+                  title: Text(item['title']),
+                  subtitle: Text("ID: ${item['id']}"),
+                );
+              }),
+    );
+  }
+}
